91422b4c9d27c88b3b0fb5904fc0f86cb8a3cf19
kyher <kyle.heron@pm.me>
2026-02-27 17:45:56 +0000

    Add basic prisma / db interaction

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,19 @@
 import cors from "cors";
 import express from "express";
+import { prisma } from "./lib/prisma";
 const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const questions = await prisma.question.findMany();
+  res.send(questions);
 });
 
 app.listen(port, () => {
